c9f5ceb3be03c44f5aff2ece77a77e99dd56f034
juanmanuelbyc <jfdiaza@unal.edu.co>
2022-09-20 06:22:30 -0400

    fix review findings

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import React from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
 import Categories from './components/categories';
 import Books from './components/books';
-import ReduceCounter from './redux/reduceCounter';
 
 function App() {
   return (
     <div className="App">
       <header>
         <h1>Bookstore CMS</h1>
-        <ReduceCounter />
         <nav>
           <Link to="/">Books</Link>
           <hr />
